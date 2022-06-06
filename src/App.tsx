4b6af1c8cd766fab101f430a37a300415d5ddc93
exsens <knyzex@mail.ru>
2022-06-06 15:32:51 +0300

    start learning typescript

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  let num: number | null = 10;
+  let str: string = 'str'
+  let arr: Array <string> = ['10', '20', 'str']
+  let arr1: string[] = ['arr', 'arr2']
+
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
